bdfc0aa1c9804683a8b309bef32148ca4b3089e3
Bozzyma <QMBoznar@outlook.com>
2022-06-17 19:12:16 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -6,6 +6,10 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -33,6 +37,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.2
 ball.dy = -0.2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player 1: 0  Player 2: 0', align='center', font=('Courier', 24, 'normal'))
 
 # Function
 def paddle_a_up():
@@ -82,10 +95,16 @@ while True:
     if ball.xcor() > 390:
         ball.setx(390)
         ball.goto(0, 0)
+        score_a += 1
+        pen.clear()
+        pen.write('Player 1: {}  Player 2: {}'.format(score_a, score_b), align='center', font=('Courier', 24, 'normal'))
     
     if ball.xcor() < -390:
         ball.setx(390)
         ball.goto(0, 0)
+        score_b += 1
+        pen.clear()
+        pen.write('Player 1: {}  Player 2: {}'.format(score_a, score_b), align='center', font=('Courier', 24, 'normal'))
 
 # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
